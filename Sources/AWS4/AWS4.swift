c3bc4a988f51c283bf19a69a9aba29246581b816
Matthias Reichmann <matthias.reichmann@icloud.com>
2022-08-14 19:20:01 +0200

    feat: add AWS4 class with basic support for IAM

--- Sources/AWS4/AWS4.swift
+++ Sources/AWS4/AWS4.swift
@@ -1,6 +1,170 @@
-public struct AWS4 {
-    public private(set) var text = "Hello, World!"
+import CryptoKit
+import Foundation
 
-    public init() {
+public class AWS4 {
+    let region: String
+    let service: String
+    let accessKeyId: String
+    let secretAccessKey: String
+    
+    init(service: String, region: String, accessKeyId: String, secretAccessKey: String) {
+        self.accessKeyId = accessKeyId
+        self.secretAccessKey = secretAccessKey
+        self.region = region
+        self.service = service
+    }
+    
+    init(service: String, region: String) {
+        self.accessKeyId = Bundle.main.infoDictionary?["AWS_ACCESSKEYID"] as! String
+        self.secretAccessKey = Bundle.main.infoDictionary?["AWS_SECRETACCESSKEY"] as! String
+        self.region = region
+        self.service = service
+    }
+    
+    init(service: String) {
+        self.accessKeyId = Bundle.main.infoDictionary?["AWS_ACCESSKEYID"] as! String
+        self.secretAccessKey = Bundle.main.infoDictionary?["AWS_SECRETACCESSKEY"] as! String
+        self.region = Bundle.main.infoDictionary?["AWS_REGION"] as! String
+        self.service = service
+    }
+    
+    private var headers: [String] {
+        ["Host", "X-Amz-Date", "Content-Type"].map { $0.lowercased() }.sorted { $0 < $1 }
+    }
+    
+    private var signedHeaders: String {
+        headers.map { $0.lowercased() }.joined(separator: ";")
+    }
+    
+    func sign(request: URLRequest, date: Date = Date()) -> URLRequest {
+        var req = request
+        
+        // 1. add host to headers
+        if let h = req.url?.host {
+            req.addValue(h, forHTTPHeaderField: "Host")
+        }
+        
+        // 2. add date to headers
+        req.addValue(iso8601String(date: date), forHTTPHeaderField: "X-Amz-Date")
+        
+        // 3. add authorization header
+        req.addValue(authHeader(request: req, date: date), forHTTPHeaderField: "Authorization")
+        
+        return req
+    }
+    
+    internal func authHeader(request req: URLRequest, date: Date) -> String {
+        return [
+            "AWS4-HMAC-SHA256",
+            "Credential=\(accessKeyId)/\(credentialScope(date: date)),",
+            "SignedHeaders=\(signedHeaders),",
+            "Signature=\(signatureFor(request: req, date: date))"
+        ].joined(separator: " ")
+    }
+    
+    internal func toSign(request req: URLRequest, date: Date) -> String {
+        return [
+            "AWS4-HMAC-SHA256",
+            req.value(forHTTPHeaderField: "X-Amz-Date")!,
+            credentialScope(date: date),
+            sha256(string: canonical(request: req)),
+        ].joined(separator: "\n")
+    }
+    
+    internal func signatureFor(request req: URLRequest, date: Date) -> String {
+        let k = signatureKey(date: date).map { String(format: "%02hhx", $0) }.joined()
+        print("key: \(k)")
+        
+        return AWS4.hmac(key: signatureKey(date: date),
+                         data: toSign(request: req, date: date))
+            .map { String(format: "%02hhx", $0) }.joined()
+    }
+    
+    internal func canonical(request req: URLRequest) -> String {
+        var can: [String] = []
+        // Docs: https://docs.aws.amazon.com/en_us/general/latest/gr/sigv4-create-canonical-request.html
+        
+        // 1. add method
+        can.append((req.httpMethod ?? "GET").uppercased())
+        
+        // 2. add uri
+        if let p = req.url?.path, p.count > 0 && p != "/" {
+            can.append(p.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)?
+                .addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? "/")
+        } else {
+            can.append("/")
+        }
+        
+        // 3. add query string
+        if let q = req.url?.query {
+            can.append(q)
+        }
+        
+        // 4. add headers
+        for header in headers {
+            guard let value = req.value(forHTTPHeaderField: header) else { continue }
+            can.append("\(header.lowercased()):\(condenseWhitespace(in: value))")
+        }
+        can.append("")
+        
+        // 5. add signed headers
+        can.append(signedHeaders)
+        
+        // 6. add payload
+        if let b = req.httpBody {
+            can.append(sha256(data: b).lowercased())
+        } else {
+            can.append(sha256(data: Data("".utf8)).lowercased())
+        }
+        
+        return can.joined(separator: "\n")
+    }
+    
+    private func credentialScope(date: Date) -> String {
+        return "\(dateString(date: date))/\(region)/\(service)/aws4_request"
+    }
+    
+    internal static func hmac(key secret: Data, data: String) -> Data {
+        let key = SymmetricKey(data: secret)
+        
+        let signature = HMAC<SHA256>.authenticationCode(for: Data(data.utf8), using: key)
+        return Data(signature)
+    }
+    
+    internal func signatureKey(date: Date) -> Data {
+        let h1 = AWS4.hmac(key: "AWS4\(secretAccessKey)".data(using: .utf8)!, data: dateString(date: date))
+        let h2 = AWS4.hmac(key: h1, data: region)
+        let h3 = AWS4.hmac(key: h2, data: service)
+        
+        return AWS4.hmac(key: h3, data: "aws4_request")
+    }
+    
+    // source: https://stackoverflow.com/questions/33058676/how-to-remove-multiple-spaces-in-strings-with-swift-2
+    private func condenseWhitespace(in s: String) -> String {
+        let components = s.components(separatedBy: .whitespacesAndNewlines)
+        return components.filter { !$0.isEmpty }.joined(separator: " ")
+    }
+
+    private func sha256(string: String) -> String {
+        return sha256(data: string.data(using: .utf8)!)
+    }
+    
+    private func sha256(data: Data) -> String {
+        let h = SHA256.hash(data: data)
+        return h.compactMap { String(format: "%02x", $0) }.joined()
+    }
+    
+    private func dateString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        
+        return formatter.string(from: date)
+    }
+    
+    private func iso8601String(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd'T'HHmmss'Z'"
+        
+        return formatter.string(from: date)
     }
 }
--- Tests/AWS4Tests/AWS4Tests.swift
+++ Tests/AWS4Tests/AWS4Tests.swift
@@ -2,10 +2,151 @@ import XCTest
 @testable import AWS4
 
 final class AWS4Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AWS4().text, "Hello, World!")
+    private func fromISO8601(_ s: String) -> Date {
+        let iso = DateFormatter()
+        iso.dateFormat = "yyyyMMdd'T'HHmmss'Z'"
+        
+        return iso.date(from: s)!
+    }
+    
+    func testCanonicalRequest() throws {
+        let url = URL(string: "https://iam.amazonaws.com/?Action=ListUsers&Version=2010-05-08")!
+        var req = URLRequest(url: url)
+        req.addValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        let aws = AWS4(service: "iam",
+                       region: "us-east-1",
+                       accessKeyId: "AKIDEXAMPLE",
+                       secretAccessKey: "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY")
+        let signed = aws.sign(request: req, date: fromISO8601("20150830T123600Z"))
+        
+        let canonical = """
+        GET
+        /
+        Action=ListUsers&Version=2010-05-08
+        content-type:application/x-www-form-urlencoded; charset=utf-8
+        host:iam.amazonaws.com
+        x-amz-date:20150830T123600Z
+
+        content-type;host;x-amz-date
+        e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+        """
+        
+        XCTAssertEqual(aws.canonical(request: signed), canonical)
+    }
+    
+    func testToSign() throws {
+        let url = URL(string: "https://iam.amazonaws.com/?Action=ListUsers&Version=2010-05-08")!
+        var req = URLRequest(url: url)
+        req.addValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        let date = fromISO8601("20150830T123600Z")
+        
+        let aws = AWS4(service: "iam",
+                       region: "us-east-1",
+                       accessKeyId: "AKIDEXAMPLE",
+                       secretAccessKey: "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY")
+        let signed = aws.sign(request: req, date: date)
+        
+        let toSign = """
+        AWS4-HMAC-SHA256
+        20150830T123600Z
+        20150830/us-east-1/iam/aws4_request
+        f536975d06c0309214f805bb90ccff089219ecd68b2577efef23edd43b7e1a59
+        """
+        
+        XCTAssertEqual(aws.toSign(request: signed, date: date), toSign)
+    }
+    
+    func testHMAC1() throws {
+        let h1 = AWS4.hmac(key: "AWS4wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY".data(using: .utf8)!,
+                           data: "20150830")
+            .map { String(format: "%02hhx", $0) }.joined()
+        
+        XCTAssertEqual(h1, "0138c7a6cbd60aa727b2f653a522567439dfb9f3e72b21f9b25941a42f04a7cd")
+    }
+    
+    func testHMAC2() throws {
+        let h1 = AWS4.hmac(key: "AWS4wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY".data(using: .utf8)!,
+                           data: "20150830")
+        let h2 = AWS4.hmac(key: h1, data: "us-east-1")
+            .map { String(format: "%02hhx", $0) }.joined()
+        
+        XCTAssertEqual(h2, "f33d5808504bf34812e5fade63308b424b244c59189be2a591dd2282c7cb563f")
+    }
+    
+    func testHMAC3() throws {
+        let h1 = AWS4.hmac(key: "AWS4wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY".data(using: .utf8)!,
+                           data: "20150830")
+        let h2 = AWS4.hmac(key: h1, data: "us-east-1")
+        let h3 = AWS4.hmac(key: h2, data: "iam")
+            .map { String(format: "%02hhx", $0) }.joined()
+        
+        XCTAssertEqual(h3, "199e1f48c602a5ae77ce26a46906920e76fc8427aeaa53da643646fcda1ccfb0")
+    }
+    
+    func testHMAC4() throws {
+        let h1 = AWS4.hmac(key: "AWS4wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY".data(using: .utf8)!,
+                           data: "20150830")
+        let h2 = AWS4.hmac(key: h1, data: "us-east-1")
+        let h3 = AWS4.hmac(key: h2, data: "iam")
+        let h4 = AWS4.hmac(key: h3, data: "aws4_request")
+            .map { String(format: "%02hhx", $0) }.joined()
+        
+        XCTAssertEqual(h4, "c4afb1cc5771d871763a393e44b703571b55cc28424d1a5e86da6ed3c154a4b9")
+    }
+    
+    func testSignatureKey() throws {
+        let url = URL(string: "https://iam.amazonaws.com/?Action=ListUsers&Version=2010-05-08")!
+        var req = URLRequest(url: url)
+        req.addValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        let aws = AWS4(service: "iam",
+                       region: "us-east-1",
+                       accessKeyId: "AKIDEXAMPLE",
+                       secretAccessKey: "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY")
+        
+        let key = aws.signatureKey(date: fromISO8601("20150830T123600Z"))
+            .map { String(format: "%02hhx", $0) }.joined()
+        
+        XCTAssertEqual(key, "c4afb1cc5771d871763a393e44b703571b55cc28424d1a5e86da6ed3c154a4b9")
+    }
+    
+    func testSignature() throws {
+        let url = URL(string: "https://iam.amazonaws.com/?Action=ListUsers&Version=2010-05-08")!
+        var req = URLRequest(url: url)
+        req.addValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        let aws = AWS4(service: "iam",
+                       region: "us-east-1",
+                       accessKeyId: "AKIDEXAMPLE",
+                       secretAccessKey: "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY")
+        let signed = aws.sign(request: req, date: fromISO8601("20150830T123600Z"))
+        
+        let signature = "5d672d79c15b13162d9279b0855cfba6789a8edb4c82c400e06b5924a6f2b5d7"
+        
+        XCTAssertEqual(aws.signatureFor(request: signed, date: fromISO8601("20150830T123600Z")), signature)
+    }
+    
+    func testIAMAuthorizationHeader() throws {
+        let url = URL(string: "https://iam.amazonaws.com/?Action=ListUsers&Version=2010-05-08")!
+        var req = URLRequest(url: url)
+        req.addValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        let aws = AWS4(service: "iam",
+                       region: "us-east-1",
+                       accessKeyId: "AKIDEXAMPLE",
+                       secretAccessKey: "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY")
+        let signed = aws.sign(request: req, date: fromISO8601("20150830T123600Z"))
+        
+        let header = "AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20150830/us-east-1/iam/aws4_request, SignedHeaders=content-type;host;x-amz-date, Signature=5d672d79c15b13162d9279b0855cfba6789a8edb4c82c400e06b5924a6f2b5d7"
+        
+        XCTAssertEqual(aws.authHeader(request: signed, date: fromISO8601("20150830T123600Z")), header)
+    }
+    
+    func testESAuthorizationHeader() throws {
+        let url = URL(string: "https://search-test-xxxxxxxxxxxxxxxxxxxxxxxxxx.eu-central-1.es.amazonaws.com")!
+        var req = URLRequest(url: url)
+        req.addValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
     }
 }
